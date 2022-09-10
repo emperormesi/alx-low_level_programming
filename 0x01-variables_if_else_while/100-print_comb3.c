17498c01f20eeb9984b591429701faf80af918ff
Ahaoma <nkems89@gmail.com>
2022-09-09 19:00:01 -0700

    New Commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,10 +19,8 @@ int main(void)
 			if (j > i)
 			{
 				putchar((i / 10) + '0');
-				putchar((j % 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
 				if (i != 89)
 				{
 					putchar(',');
