c9c9d3bb0e2d9cf1b75179f8118aa5ab5cef5012
Ahaoma <nkems89@gmail.com>
2022-09-12 09:58:54 -0700

    New Message

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int i, j, k;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (i = 0; i < 8; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (j = i + 1; j < 9; j++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			for (k = j + 1; k < 10; k++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+				if (i == 7 && j == 8 && k == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
